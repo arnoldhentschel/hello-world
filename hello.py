d49c8544d8fb740ec5142c18dbc1843e0756f26f
arnoldhentschel <46100814+arnoldhentschel@users.noreply.github.com>
2024-09-28 20:30:08 +0000

    erweiterung

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
-print( 'hello world! ');
+import time, locale;
+name = input( 'Geben Sie bitte Ihren Namen ein: ');
+print( 'Hallo %s! ', name);
+# locale.setlocale( locale.LC_ALL, 'de_DE'); #linux, macos
+# locale.setlocale( locale.LC_ALL, 'de_DE'); #windows
+thetime = time.strftime( 'heute ist %A, der %d.%B.');
+print( thetime)
+
