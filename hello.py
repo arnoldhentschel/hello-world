b1b8b2bcef2f952b09a53ca4f1e84d36b1391ddb
arnoldhentschel <46100814+arnoldhentschel@users.noreply.github.com>
2024-10-02 14:56:25 +0000

    stringtests eingebaut

--- hello.py
+++ hello.py
@@ -1,8 +1,27 @@
-import time, locale;
-name = input( 'Geben Sie bitte Ihren Namen ein: ');
-print( 'Hallo %s! ', name);
-# locale.setlocale( locale.LC_ALL, 'de_DE'); #linux, macos
-# locale.setlocale( locale.LC_ALL, 'de_DE'); #windows
-thetime = time.strftime( 'heute ist %A, der %d.%B.');
-print( thetime)
+import time, locale, requests;
 
+mach01 = False;
+
+if (mach01):
+    deinname = input( 'Geben Sie bitte Ihren Namen ein: ');
+    print( 'Hallo %s! ' %deinname);
+    # locale.setlocale( locale.LC_ALL, 'de_DE'); #linux, macos
+    # locale.setlocale( locale.LC_ALL, 'de_DE'); #windows
+    thetime = time.strftime( 'heute ist %A, der %d.%B.');
+    print( thetime)
+
+    response = requests.get( 'https://httpbin.org/get?q=123');
+    print( response.json());
+
+s = "ich bin lang";
+print( "Die Zeichenkette <" + s + ">")
+print( "s[4:7]");
+print( s[4:7]);
+print( "s[4:]");
+print( s[4:]);
+print( "s[:3]");
+print( s[:3]);
+print( "s[-4:]");
+print( s[-4:]);
+print( "s[-4:-2]");
+print( s[-4:-2]);
