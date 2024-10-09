cea2afb4ec7107625657f6a04e13670245d1137d
arnoldhentschel <46100814+arnoldhentschel@users.noreply.github.com>
2024-10-09 19:15:27 +0000

    Listen begonnen

--- lists.py
+++ lists.py
@@ -1,1 +1,9 @@
+import sys;
 # Beispiel fuer Listenverarbeitung
+def move (y, x):
+    #print("\033[%d;%dH" % (y, x));
+    sys.stdout.write("\033[%d;%dH" % (y, x));
+
+for i in range( 255,1055):
+    #   move( i, i);
+    print( str(i)+ ':' + chr(i));
